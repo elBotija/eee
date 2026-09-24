1471f6dc91b05b703e6623ee1e6ad04f815042b4
elbotija <mauroenpixels@gmail.com>
2026-09-24 18:18:28 -0300

    fix compatibilidad

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    legacy({
+      targets: ['ios >= 9'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
   ],
   base: '/eee/'
 })
